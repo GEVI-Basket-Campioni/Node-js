7d9d07df986d075add5adea2678ea547d624daaf
Mirko Onorato <85618159+mirkoSkF@users.noreply.github.com>
2024-03-19 12:00:21 +0100

    Update gestione.js

--- gestione.js
+++ gestione.js
@@ -6,51 +6,117 @@ var input = require("readline-sync");
 //lista vuota
 var database = [];
 
-function menu(){
+function menu() {
     console.log("####MENU BELLO°°°°");
     console.log("1)Inserisci utente");
     console.log("2)Visualizza utenti");
+    console.log("3)Cerca utente");
+    console.log("4)Modifica utente");
+    console.log("5)Cancella utente");
+    console.log("6)Esci");
 }
 
-//"1;pino;mauro"
-function inserisciUtente(){
+function inserisciUtente() {
     var id = input.question("Inserisci id: ");
     var nome = input.question("Inserisci nome: ");
     var cognome = input.question("Inserisci cognome: ");
-    var utente = id+";"+nome+";"+cognome;
+    //"1;pino;mauro" utente nel formato .csv
+    var utente = id + ";" + nome + ";" + cognome;
     database.push(utente);
+    console.log("Inserimento avvenuto con successo!!!");
 }
 
-function visualizzaUtenti(){
-    for(var indice=0; indice<database.length; indice++){
-        console.log(database[indice]);
-    }
-}
-
-function visualizzaUtentiBello(){
-    for(var indice=0; indice<database.length; indice++){
-        var utenteCorrente = database[indice];
+function visualizzaUtenti() {
+    for (var i = 0; i < database.length; i++) {
+        var utenteCorrente = database[i];
         //split mi da un array di campi
         var campiEstrapolati = utenteCorrente.split(";");
         id = campiEstrapolati[0];
         nome = campiEstrapolati[1];
         cognome = campiEstrapolati[2];
         console.log("####SCHEDA UTENTE°°°°");
-        console.log("1)Id: "+id);
-        console.log("2)Nome: "+nome);
-        console.log("3)Cognome: "+cognome);
+        console.log("1)Id: " + id);
+        console.log("2)Nome: " + nome);
+        console.log("3)Cognome: " + cognome);
     }
 }
 
-do{
+function cercaUtente() {
+    var id_ricerca = parseInt(input.question("Dammi id ricerca: "));
+    for (var i = 0; i < database.length; i++) {
+        var utenteCorrente = database[i];
+        var campi = utenteCorrente.split(";");
+        if (id_ricerca === parseInt(campi[0])) {
+            console.log("####SCHEDA UTENTE°°°°");
+            console.log("1)Id: " + campi[0]);
+            console.log("2)Nome: " + campi[1]);
+            console.log("3)Cognome: " + campi[2]);
+            break;
+        }
+    }
+}
+
+function modificaUtente() {
+    var id_ricerca = parseInt(input.question("Dammi id ricerca: "));
+    for (var i = 0; i < database.length; i++) {
+        var utenteCorrente = database[i];
+        var campi = utenteCorrente.split(";");
+        if (id_ricerca === parseInt(campi[0])) {
+            console.log("####DATI CORRENTI DELL'UTENTE°°°°");
+            console.log("1)Id: " + campi[0]);
+            console.log("2)Nome: " + campi[1]);
+            console.log("3)Cognome: " + campi[2]);
+            var nome_nuovo = input.question("Inserisci il nome o premi enter per skippare: ");
+            if(nome_nuovo==""){
+                nome_nuovo=campi[1];
+            }
+            var cognome_nuovo = input.question("Inserisci il cognome o premi enter per skippare: ");
+            if(cognome_nuovo==""){
+                cognome_nuovo=campi[2];
+            }
+            var nuovo_utente =campi[0]+";"+nome_nuovo+";"+cognome_nuovo;
+            database[i]=nuovo_utente;
+            break;
+        }
+    }
+}
+
+function cancellaUtente() {
+    var id_ricerca = parseInt(input.question("Dammi id ricerca: "));
+    for (var i = 0; i < database.length; i++) {
+        var utenteCorrente = database[i];
+        var campi = utenteCorrente.split(";");
+        if (id_ricerca === parseInt(campi[0])) {
+            database.splice(i,1);
+            break;
+        }
+    }
+}
+
+
+
+do {
     menu();
     var scelta = parseInt(input.question("Scegli una opzione: "));
-    switch(scelta){
-        case 1: 
+    switch (scelta) {
+        case 1:
             inserisciUtente();
             break;
         case 2:
-            visualizzaUtentiBello();
+            visualizzaUtenti();
             break;
+        case 3:
+            cercaUtente();
+            break;
+        case 4:
+            modificaUtente();
+            break;
+        case 5:
+            cancellaUtente();
+            break;
+        case 6:
+            break;
+        default:
+            console.log("Scelta non valida!");
     }
-}while(scelta!=6);
+} while (scelta != 6);
